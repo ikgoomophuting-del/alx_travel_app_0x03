bf02393a333a6a1e17bcf396c29c1b9dc09d4375
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-02 17:47:55 +0200

    Update views.py

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,7 +1,12 @@
-from rest_framework import viewsets
+from django.http import JsonResponse
+from .models import Payment
+from .tasks import send_payment_confirmation_email
+from .utils.payment_gateway import verify_payment
 from .models import Booking
 from .serializers import BookingSerializer
 from .tasks import send_booking_confirmation_email
+from rest_framework import viewsets
+
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
@@ -12,3 +17,21 @@ class BookingViewSet(viewsets.ModelViewSet):
         # Trigger async email
         send_booking_confirmation_email.delay(booking.customer.email, booking.id)
       
+    def verify_payment_view(request):
+    reference = request.GET.get("tx_ref")
+    email = request.GET.get("email", "user@example.com")
+
+    payment = Payment.objects.filter(booking_reference=reference).first()
+    if not payment:
+        return JsonResponse({"error": "Payment not found"}, status=404)
+
+    response_data = verify_payment(reference)
+    status = response_data.get("data", {}).get("status", "failed")
+
+    payment.status = "Completed" if status == "success" else "Failed"
+    payment.save()
+
+    send_payment_confirmation_email.delay(email, reference, payment.amount, payment.status)
+    return JsonResponse({"status": payment.status})
+    
+
