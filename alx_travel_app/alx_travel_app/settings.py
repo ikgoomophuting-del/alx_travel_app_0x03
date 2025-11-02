beddfba1dd3cc324ab31492588fa6d08c3b71a71
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-02 17:07:16 +0200

    Update settings.py

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -1,1 +1,8 @@
 
+# Celery Configuration
+CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Johannesburg'
